801c78b63e78e44cdad51f898e9cc1ee54bb05ef
AIINIRII <aiinirii@163.com>
2025-08-25 15:12:50 -0400

    update: 2025/08/25

--- Leetcode/src/Q2130PairSum.cpp
+++ Leetcode/src/Q2130PairSum.cpp
@@ -20,8 +20,48 @@ class Solution {
 public:
     int pairSum(ListNode *head) {
         // 1. 找到中间点
+        ListNode *node1 = head, *node2 = head->next->next;
+        while (node2 != nullptr) {
+            node1 = node1->next;
+            node2 = node2->next->next;
+        }
         // 2. 截断后半部分链表
+        ListNode *toReverseList = node1->next;
+        node1->next = nullptr;
         // 3. 反转后半部分链表
+        if (toReverseList == nullptr || toReverseList->next == nullptr) {
+        } else if (toReverseList->next->next == nullptr) {
+            ListNode *first = toReverseList, *second = toReverseList->next;
+            toReverseList = second;
+            second->next = first;
+            first->next = nullptr;
+        } else {
+            ListNode *newList = toReverseList;
+            ListNode *second = toReverseList->next, *third = toReverseList->next->next;
+            while (third != nullptr) {
+                second->next = newList;
+                newList = second;
+                second = third;
+                third = third->next;
+            }
+            second->next = newList;
+            newList = second;
+            toReverseList = newList;
+        }
         // 4. 双指针同时遍历两个链表，然后计算最大值
+        node1 = head;
+        node2 = toReverseList;
+
+        int maxSum = 0;
+        while (node1 != nullptr) {
+            int sum = node1->val + node2->val;
+            if (sum > maxSum) {
+                maxSum = sum;
+            }
+            node1 = node1->next;
+            node2 = node2->next;
+        }
+
+        return maxSum;
     }
 };
